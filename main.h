6a143bd6e6a17a0531987eee7486de99425be29b
KateM27 <mamocatherine@gmail.com>
2022-07-11 16:06:58 +0300

    prototypes

--- main.h
+++ main.h
@@ -15,4 +15,12 @@ typedef struct print
 
 int _putchar(char c);
 int _printf(const char *format, ...);
+int print_c(va_list c);
+int print_s(va_list s);
+int print_i(va_list i);
+int print_d(va_list d);
+int print_u(va_list u);
+int print_b(va_list b);
+int print_o(va_list o);
+int print_x(va_list x);
 #endif
