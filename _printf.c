0da42970dbb1508099219c3063ac062b01394491
KateM27 <mamocatherine@gmail.com>
2022-07-11 15:26:28 +0300

    prototypes

---
+++ _printf.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * _printf: produces output according to a format
+ * @format: specifiers to identify
+ * Return: length of string
+ */
+int _printf(const char *format, ...)
+{
+
