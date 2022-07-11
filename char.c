e0bae0682ff151114a546f50cbc5352a041c4804
mike-nyange <micknyange@gmail.com>
2022-07-11 19:20:11 +0300

    Update char.c

--- char.c
+++ char.c
@@ -34,4 +34,34 @@ int print_s(va_list s)
 	}
 	return (count);
 }
+/**
+ * print_S - prints a string and nonprintable character ascii values
+ * @S: string to print
+ *
+ * Return: number of chars printed
+ */
+int print_S(va_list S)
+{
+	unsigned int i;
+	int count = 0;
+	char *str = va_arg(S, char *);
 
+	if (str == NULL)
+		str = "(null)";
+	for (i = 0; str[i]; i++)
+	{
+		if (str[i] < 32 || str[i] >= 127)
+		{
+			_putchar('\\');
+			_putchar('x');
+			count += 2;
+			count += hex_print(str[i]);
+		}
+		else
+		{
+			_putchar(str[i]);
+			count++;
+		}
+	}
+	return (count);
+}
