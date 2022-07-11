5e0e1610f7a126d6eb4c13c8da738c65be5fac23
mike-nyange <micknyange@gmail.com>
2022-07-11 12:22:30 +0000

    initial _printf.c

---
+++ _printf.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * _printf: produces output according to a format.
+ * @format: specifier to look for.
+ * Return: length of string
+ */
+
+int _printf(const char *format, ...)
+{
