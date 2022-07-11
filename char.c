974ebcaa133645cc2e714792331beb0f81adbcf1
KateM27 <99427213+KateM27@users.noreply.github.com>
2022-07-11 17:02:45 +0300

    Update char.c

--- char.c
+++ char.c
@@ -14,3 +14,24 @@ int print_c(va_list c)
 	_putchar(ch);
 	return (1);
 }
+
+/**
+ * print_s - prints a string
+ * @s: string to print
+ *
+ * Return: number of chars printed
+ */
+int print_s(va_list s)
+{
+	int count;
+	char *str = va_arg(s, char *);
+
+	if (str == NULL)
+		str = "(null)";
+	for (count = 0; str[count]; count++)
+	{
+		_putchar(str[count]);
+	}
+	return (count);
+}
+
