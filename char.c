8e9896710cb5206d96ef99a3edf1e3937fd569dd
KateM27 <99427213+KateM27@users.noreply.github.com>
2022-07-11 16:19:03 +0300

    Create char.c

---
+++ char.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * print_c - prints a char
+ * @c: char to print
+ *
+ * Return: always 1
+ */
+int print_c(va_list c)
+{
+	char ch = (char)va_arg(c, int);
+
+	_putchar(ch);
+	return (1);
+}
