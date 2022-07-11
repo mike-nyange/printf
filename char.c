68c77db26791e81bba45cb8d5f5a40e12a432d31
KateM27 <99427213+KateM27@users.noreply.github.com>
2022-07-11 19:42:36 +0300

    update character file

--- char.c
+++ char.c
@@ -65,3 +65,27 @@ int print_S(va_list S)
 	}
 	return (count);
 }
+
+/**
+ * print_r - prints a string in reverse
+ * @r: string to print
+ *
+ * Return: number of characters printed
+ */
+int print_r(va_list r)
+{
+	char *str;
+	int i, count = 0;
+
+	str = va_arg(r, char *);
+	if (str == NULL)
+		str = ")llun(";
+	for (i = 0; str[i]; i++)
+		;
+	for (i -= 1; i >= 0; i--)
+	{
+		_putchar(str[i]);
+		count++;
+	}
+	return (count);
+}
