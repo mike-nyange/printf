7321c007ac07a8027f6e86cee2cab670efe8fb50
mike-nyange <micknyange@gmail.com>
2022-07-12 12:22:50 +0300

    Update char.c

--- char.c
+++ char.c
@@ -89,3 +89,25 @@ int print_r(va_list r)
 	}
 	return (count);
 }
+/**
+ * handle_percent - writes characters after a % if they
+ * don't match anything in the struct
+ * @ch1: the first char passed (always %)
+ * @ch2: the char after the %
+ * Return: 1 if ch2 is a % and 2 is ch2 is anything else
+ */
+
+int handle_percent(char ch1, char ch2)
+{
+	if (ch2 == '%')
+	{
+		write(1, &ch2, 1);
+		return (1);
+	}
+	else
+	{
+		write(1, &ch1, 1);
+		write(1, &ch2, 1);
+		return (2);
+	}
+}
