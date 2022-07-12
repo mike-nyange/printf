1166944bf52e6081fc736d6feef0c656d847427c
mike-nyange <micknyange@gmail.com>
2022-07-12 14:51:53 +0300

    Update binary_address.c

--- binary_address.c
+++ binary_address.c
@@ -44,34 +44,24 @@ int print_b(va_list list)
  */
 int print_p(va_list list)
 {
-	char *hex;
-	char *s = "(nil)";
+	char *hex, char *s = "(nil)";
 	unsigned long int add = (unsigned long int)va_arg(list, void *);
 	int c = 0, i, temp, len;
 
-	if (add == 0)
-	{
-		for (i = 0; s[i]; i++)
-		{
+	if (add == 0) {
+		for (i = 0; s[i]; i++) {
 			_putchar(s[i]);
-			c++;
-		}
-		return (c);
-	}
+			c++;}
+		return (c);}
 	_putchar('0');
 	_putchar('x');
-	c = 2;
-	i = 0;
+	c = 2, i = 0;
 	len = base_len(add, 16);
 	hex = malloc((sizeof(char) * len) + 1);
-	if (!hex)
-	{
-		for (i = 0; s[i]; i++)
-		{
-			_putchar(s[i]);
-		}
-		return (5);
-	}
+	if (!hex) {
+		for (i = 0; s[i]; i++) {
+			_putchar(s[i]); }
+		return (5); }
 	while (add > 0)
 	{
 		temp = add % 16;
