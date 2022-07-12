4880e47479a0e792ab2ed37233445dbb31f1f326
mike-nyange <micknyange@gmail.com>
2022-07-12 15:10:17 +0300

    Update binary_address.c

--- binary_address.c
+++ binary_address.c
@@ -31,11 +31,11 @@ int print_b(va_list list)
 	{
 	free(str);
 	return (len); }
-
-/**
- *print_p - prints an address
- *@list:list to increment
- *Return:count of characters
+	
+	/**
+ *print_p - prints an address 
+ *@list: list to increment
+ *Return: count of characters
  */
 int print_p(va_list list)
 {
@@ -47,19 +47,19 @@ int print_p(va_list list)
 	if (add == 0)
 	{
 		for (i = 0; s[i]; i++)
-		{
 			_putchar(s[i]);
-			c++; }
+			c++;
 		return (c); }
 	_putchar('0');
 	_putchar('x');
 	c = 2, i = 0;
 	len = base_len(add, 16);
-	hex = malloc((sizeof(char) * len) + 1);
+	hex = malloc((sizeof(char) *len) + 1);
 	if (!hex)
 	{
 		for (i = 0; s[i]; i++)
-			_putchar(s[i]);
+		{
+			_putchar(s[i]); }
 		return (5); }
 	while (add > 0)
 	{
@@ -72,8 +72,7 @@ int print_p(va_list list)
 		{
 			hex[i] = temp + 48;
 			i++; }
-		add /= 16;
-	}
+		add /= 16; }
 	rev_string(hex);
 	for (i = 0; hex[i]; i++)
 	{
