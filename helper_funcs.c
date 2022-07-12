82e1dfc92b2bfd92edf6264714d3133eed5c2f01
mike-nyange <micknyange@gmail.com>
2022-07-12 23:02:11 +0300

    Update helper_funcs.c

--- helper_funcs.c
+++ helper_funcs.c
@@ -79,26 +79,3 @@ char *hex_conv(int num)
 	rev_string(str);
 	return (str);
 }
-/**
- *convert_to - convert numbers
- *@representation: char representation[] = "0123456789ABCDEF";
- *@num: num to tranasform
- *@base: base to transform num
- *Return: number into char pointer
- */
-char *convert_to(char representation[], unsigned int num, int base)
-{
-	char *ptr;
-	static char buffer[128];
-	int mod = 0;
-
-	ptr = &buffer[127];
-	*ptr = '\0';
-
-	do {
-		mod = num % base;
-		*--ptr = representation[mod];
-		num /= base;
-	} while (num != 0);
-	return (ptr);
-}
